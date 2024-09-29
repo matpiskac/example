dea4e5ba5c0b964f78e30a29658d2f2cc149ef47
Miscatonic <matija-piskac@hotmail.com>
2024-09-29 11:49:49 +0200

    Fix static mistakes

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+"""Simple echo server"""
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory=".")
 
-@app.get("/echo/{message}",  response_class=HTMLResponse)
+
+@app.get("/echo/{message}", response_class=HTMLResponse)
 async def echo(request: Request, message: str):
+    """Echo message through a template"""
+
     return templates.TemplateResponse(
         request=request, name="echo.html", context={"message": message}
     )
-
